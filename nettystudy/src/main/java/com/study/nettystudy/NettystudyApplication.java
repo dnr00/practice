9d38d6ec43db754e989784bc1165548235a33b61
dnr00 <dnr00@naver.com>
2023-06-02 17:21:07 +0900

    230602

--- nettystudy/src/main/java/com/study/nettystudy/NettystudyApplication.java
+++ nettystudy/src/main/java/com/study/nettystudy/NettystudyApplication.java
@@ -1,13 +1,16 @@
 package com.study.nettystudy;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NettystudyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NettystudyApplication.class, args);
+		SpringApplication app = new SpringApplication(NettystudyApplication.class);
+		app.setWebApplicationType(WebApplicationType.NONE);
+		app.run(args);
 	}
 
 }
---
+++ nettystudy/src/main/java/com/study/nettystudy/echo/EchoServerHandler.java
@@ -0,0 +1,25 @@
+package com.study.nettystudy.echo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * 서버 사이드 채널 처리
+ */
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ctx.write(msg);
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // 예외가 발생하면 연결을 종료합니다.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
